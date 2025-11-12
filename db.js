a6b38096da7ae1cf5a0fc8d6ea7f52ec463b1b30
Kartikeya Ranjan <kartikeyaranjan02@gmail.com>
2025-11-12 23:30:46 +0530

    fixes in db.js

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
+mongoose.connect("");
 const { Schema } = mongoose;
+const objectId = mongoose.Types.ObjectId;
 
 const userSchema = Schema({
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
 });
 
 const adminSchema = Schema({
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -20,15 +22,17 @@ const courseSchema = Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   imageUrl: { type: String, required: true },
-  creatorId: { type: Schema.ObjectId, required: true },
+  creatorId: { type: objectId, required: true },
 });
 
 const purchaseSchema = Schema({
-  courseId: { type: Schema.ObjectId, required: true },
-  userId: { type: Schema.ObjectId, required: true },
+  courseId: { type: objectId, required: true },
+  userId: { type: objectId, required: true },
 });
 
 const userModel = mongoose.model("user", userSchema);
 const adminModel = mongoose.model("admin", adminSchema);
-const courseModel = mongoose.model("user", userSchema);
+const courseModel = mongoose.model("course", courseSchema);
 const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = { userModel, adminModel, courseModel, purchaseModel };
